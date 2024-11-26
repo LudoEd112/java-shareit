07c8a28a5824acc07600735380cbbf3273ca7c2f
Дима <datakmakov@gmail.com>
2024-11-27 00:26:47 +0300

    Коммит 2, исправлены замечания

--- src/main/java/ru/practicum/shareit/exception/ErrorResponse.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorResponse.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @Data
